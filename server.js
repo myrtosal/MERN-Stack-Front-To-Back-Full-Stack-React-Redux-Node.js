77f83979fb4cad1b2fcef9c0558888db00e5dae9
myrtosal <myrtosal@hotmail.com>
2021-08-13 16:32:21 +0300

    database connected

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
 
+// Connect Database
+connectDB();
+
 app.get('/', (req, res) => {
-    res.send('API running');
+    res.send('API Running');
 });
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-    console.log('Server started on port ${PORT}');
-});
+app.listen(PORT, () => console.log(`Server Started on Port ${PORT}...`));
