159ec580b5c87c0471500d8f971c310c0355fe54
myrtosal <myrtosal@hotmail.com>
2021-08-14 13:56:41 +0300

    register user and return jwt

--- config/db.js
+++ config/db.js
@@ -2,21 +2,21 @@ const mongoose = require('mongoose');
 const config = require('config');
 const db = config.get('mongoURI');
 
-
 const connectDB = async () => {
-    try {
-        await mongoose.connect(db, {   
-            useNewUrlParser: true,
-            useUnifiedTopology: true 
-        });
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true
+    });
 
-        console.log('MongoDB Connected...');
-    } catch (err) {
-        console.error(err.message);
+    console.log('MongoDB connected...');
+  } catch (err) {
+    console.error(err.message);
 
-        // Exit proccess with failure
-        proccess.exit(1);
-    }
-}
+    // Exit proccess with failure
+    proccess.exit(1);
+  }
+};
 
 module.exports = connectDB;
--- server.js
+++ server.js
@@ -6,10 +6,10 @@ const app = express();
 // Connect Database
 connectDB();
 
-app.use(express.json({ extended: false}));
+app.use(express.json({ extended: false }));
 
 app.get('/', (req, res) => {
-    res.send('API Running');
+  res.send('API Running');
 });
 
 // Define routes
@@ -21,5 +21,5 @@ app.use('/api/posts', require('./routes/api/posts'));
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-    console.log(`Server Started on Port ${PORT}`);
+  console.log(`Server started on port ${PORT}`);
 });
