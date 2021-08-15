193da77f2e9085a14212fd6ba813bf2ac06d9ea0
myrtosal <myrtosal@hotmail.com>
2021-08-15 21:56:26 +0300

    delete experience from profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -212,4 +212,29 @@ router.put(
   }
 );
 
+// @route   DELETE api/profile/experience/:exp_id
+// @desc    Delete experience from profile
+// @access  Private
+router.delete('/experience/:exp_id', auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id });
+
+    // Get remove index
+    const removeIndex = profile.experience.map((item) => item.id).indexOf(req.params.exp_id);
+
+    if (profile.experience) {
+      profile.experience = profile.experience.filter(
+        (profileExp) => profileExp._id.toString() !== req.params.exp_id.toString()
+      );
+    }
+
+    await profile.save();
+
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server error');
+  }
+});
+
 module.exports = router;
