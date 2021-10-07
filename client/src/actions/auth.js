389c7522465aa36ab41a12308bb43c79e6463eea
myrtosal <myrtosal@hotmail.com>
2021-10-07 16:37:20 +0300

    user login

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -1,6 +1,13 @@
 import axios from 'axios';
 import { setAlert } from './alert';
-import { REGISTER_SUCCESS, REGISTER_FAIL, USER_LOADED, AUTH_ERROR } from './types';
+import {
+  REGISTER_SUCCESS,
+  REGISTER_FAIL,
+  USER_LOADED,
+  AUTH_ERROR,
+  LOGIN_SUCCESS,
+  LOGIN_FAIL
+} from './types';
 import setAuthToken from '../utils/setAuthToken';
 
 // Load user
@@ -41,11 +48,13 @@ export const register =
         type: REGISTER_SUCCESS,
         payload: res.data
       });
+
+      dispatch(loadUser());
     } catch (err) {
       const errors = err.response.data.errors;
 
       if (errors) {
-        errors.array.forEach((error) => dispatch(setAlert(error.msg, 'danger')));
+        errors.forEach((error) => dispatch(setAlert(error.msg, 'danger')));
       }
 
       dispatch({
@@ -53,3 +62,34 @@ export const register =
       });
     }
   };
+
+// Login user
+export const login = (email, password) => async (dispatch) => {
+  const config = {
+    headers: {
+      'Content-type': 'application/json'
+    }
+  };
+
+  const body = JSON.stringify({ email, password });
+
+  try {
+    const res = await axios.post('/api/auth', body, config);
+    dispatch({
+      type: LOGIN_SUCCESS,
+      payload: res.data
+    });
+
+    dispatch(loadUser());
+  } catch (err) {
+    const errors = err.response.data.errors;
+
+    if (errors) {
+      errors.forEach((error) => dispatch(setAlert(error.msg, 'danger')));
+    }
+
+    dispatch({
+      type: LOGIN_FAIL
+    });
+  }
+};
