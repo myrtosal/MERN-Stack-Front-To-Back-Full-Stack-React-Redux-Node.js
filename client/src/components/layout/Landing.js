af5a76bce4647e3d60bed6035961eec8f8540ece
myrtosal <myrtosal@hotmail.com>
2021-10-07 21:27:54 +0300

    starting on the dashboard

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -1,7 +1,13 @@
-import React from 'react';
+import React, { Redirect } from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { PropTypes } from 'prop-types';
 
-const Landing = () => {
+const Landing = ({ isAuthenticated }) => {
+  if (isAuthenticated) {
+    return <Redirect to='/dashboard' />;
+  }
+
   return (
     <section className='landing'>
       <div className='dark-overlay'>
@@ -24,4 +30,12 @@ const Landing = () => {
   );
 };
 
-export default Landing;
+Landing.porpTypes = {
+  isAuthenticated: PropTypes.bool
+};
+
+const mapStateToProps = (state) => ({
+  isAuthenticated: state.auth.isAuthenticated
+});
+
+export default connect(mapStateToProps)(Landing);
